74848e21d03c3c4407c894526fde3859f2bfaa3c
imaras <imaras@net.hr>
2023-11-15 15:43:11 +0100

    Create sample project

---
+++ ManagedLibrary/NativeExports.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ManagedLibrary
+{
+  public static unsafe class NativeExports
+  {
+    [UnmanagedCallersOnly(EntryPoint = nameof(MyEqual))]
+    public static unsafe bool MyEqual(int a, int b)
+    {
+      return EqualityComparer<int>.Default.Equals(a, b);
+    }
+  }
+}
